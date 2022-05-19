e6fcd4301d3c69eb2ffc167323df25ac6670436b
Lidar <lidar@MacBook-Pro-Lidar.local>
2022-05-19 22:03:30 +0600

    1

---
+++ backend/apps/accounts/views.py
@@ -0,0 +1,52 @@
+
+from django.shortcuts import render, redirect
+from django.views.generic import (
+    FormView, 
+    CreateView,
+    TemplateView
+    )
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from .forms import LoginForm, UserRegisterForm
+
+
+class LoginView(FormView):
+    template_name = "login.html"
+    form_class = LoginForm
+
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        email = data['email']
+        password = data['password']     
+        user = authenticate(email=email, password=password)
+        print(user.is_authenticated)
+        if user is not None:
+            if user.is_active:
+                login(self.request, user)
+                return redirect('index')
+            else:
+                return HttpResponse("Ваш аккаунт неактивен")
+        return HttpResponse("Такого юзера не существует")
+    
+
+
+
+class UserRegisterView(CreateView):
+    template_name = "register.html"
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('register_done')
+
+
+#generic - готовый классы с готовым решением,
+# для стандартных задач
+class RegisterDoneView(TemplateView):
+    template_name = "register_done.html"
+
+
+
+def user_logout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('index')
---
+++ backend/apps/product/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', IndexPage.as_view(), name='index'),
+    path('getSubcategory/', get_subcategory, name="get_subcategory"),
+    path('list/product/', ProductListView.as_view(), name='product_list'),
+    path('detail/product/<int:pk>/',ProductDetailView.as_view(), name="product_detail"),
+    path('list/category/<slug:slug>/', ProductListView.as_view(), name="category_products"),
+    path('list/category/<slug:slug>/<slug:subcategory_slug>/', ProductListView.as_view(), name="subcategory_products"),
+]
+
+# 'list/category/avtotovary/'
+#'list/category/obuv/jenskaya/'
